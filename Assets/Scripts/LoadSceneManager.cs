4d2f6ae193049dc0339ddd1ea80db80f92100da8
TaufikHakim <93898691+Fikha203@users.noreply.github.com>
2022-10-26 15:16:30 +0700

    Scene menu +  Transisi crossfade
    Object LoadSCeneManageeer prefab dipakai di tiap scene berfungsi untuk mengatur perpindahan scene, fitur baru tombol play akan pindah ke scenen dituju, dan back ke scene sebelumnya

---
+++ Assets/Scripts/LoadSceneManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneManager : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f; //lama waktu transisi
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // pakai method ini pada tombol pindah scene
+    public void BackScene()
+    {
+        // Load scene sebelum scene yang sedang aktif
+        StartCoroutine(Scene(SceneManager.GetActiveScene().buildIndex - 1));
+    }
+    public void LoadScene(string str) //String nama scene yangg dituju
+    {
+        Debug.Log("Pindah");
+        StartCoroutine(Scene(str));
+    }
+
+    IEnumerator Scene(string str)
+    {
+       transition.SetTrigger("Start");
+       yield return new WaitForSeconds(transitionTime);
+       SceneManager.LoadScene(str); 
+    }
+
+    IEnumerator Scene(int index)
+    {
+       transition.SetTrigger("Start");
+       yield return new WaitForSeconds(transitionTime);
+       SceneManager.LoadScene(index); 
+    }
+}
