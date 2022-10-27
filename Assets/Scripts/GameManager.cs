4a5d12d5b00f065c416e0b77e8213f472675c844
QaorVa <100664948+QaorVa@users.noreply.github.com>
2022-10-27 17:36:25 +0700

    Health Bar + Result Screen
    nambahin health bar, munculin result screen kalo darahnya abis, tapi gamenya masih jalan walopun darahnya abis, sama game endnya jg belom bisa

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour
 {
 
     public static bool gameIsPaused = false;
+    public static bool isFailed = false;
+
     public GameObject pauseMenu;
     public GameObject scoreManager;
     public AudioSource audioSource;
@@ -28,6 +30,15 @@ public class GameManager : MonoBehaviour
     public TMP_Text mulText;
     public TMP_Text comboText;
 
+    public float totalNotes;
+    public float okayHits;
+    public float goodHits;
+    public float perfectHits;
+    public float missedHits;
+
+    public GameObject resultScreen;
+    public TMP_Text percentHitText, okaysText, goodsText, perfectsText, missesText, rankText, finalScoreText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -42,12 +53,38 @@ public class GameManager : MonoBehaviour
     {
 
         // mulai saat mencet any key 
-        if(Input.anyKeyDown && !gameIsPaused)
+        if(Input.anyKeyDown && !gameIsPaused && !isFailed)
         {
-           scoreManager.GetComponent<SongManager>().enabled = true;         
+           scoreManager.GetComponent<SongManager>().enabled = true;
         }
 
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(!gameIsPaused)
+        {
+            if (!Health.instance.alive && !resultScreen.activeInHierarchy)
+            {
+                Failed();
+            }
+            /*
+            else if (!audioSource.isPlaying && !resultScreen.activeInHierarchy && Health.instance.alive)
+            {
+                resultScreen.SetActive(true);
+                scoreText.enabled = false;
+                mulText.enabled = false;
+
+                okaysText.text = okayHits.ToString();
+                goodsText.text = goodHits.ToString();
+                perfectsText.text = perfectHits.ToString();
+                missesText.text = missedHits.ToString();
+                finalScoreText.text = currentScore.ToString();
+
+                float totalHit = okayHits + goodHits + perfectHits;
+                float percentHit = (totalHit / totalNotes) * 100f;
+
+                percentHitText.text = percentHit.ToString("F1") + "%";
+            } */
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape) && !resultScreen.activeInHierarchy)
         {
             if(gameIsPaused)
             {
@@ -85,6 +122,8 @@ public class GameManager : MonoBehaviour
             }
         }
 
+        totalNotes++;
+
         //menampilkan teks score, combo, dan multiplier
         comboText.text = comboTracker + "x";
 
@@ -98,24 +137,36 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log("Okay Hit");
 
+        okayHits++;
+
         currentScore += okayScore * currentMultiplier;
         noteHit();
+
+        Health.instance.Heal(1f);
     }
 
     public void GoodHit()
     {
         Debug.Log("Good Hit");
 
+        goodHits++;
+
         currentScore += goodScore * currentMultiplier;
         noteHit();
+
+        Health.instance.Heal(2f);
     }
 
     public void PerfectHit()
     {
         Debug.Log("Perfect Hit");
 
+        perfectHits++;
+
         currentScore += perfectScore * currentMultiplier;
         noteHit();
+
+        Health.instance.Heal(5f);
     }
 
     // method yang dijalankan saat note gagal untuk dihit
@@ -129,11 +180,16 @@ public class GameManager : MonoBehaviour
         //mereset nilai combo
         comboTracker = 0;
 
+        Health.instance.TakeDamage(10f);
+
         //menghilangkan teks combo jika combo = 0
         if (comboTracker == 0)
         {
             comboText.enabled = false;
         }
+
+        totalNotes++;
+        missedHits++;
 
         //menampilkan teks multiplier
         mulText.text = "MULTIPLIER: x" + currentMultiplier;
@@ -168,5 +224,26 @@ public class GameManager : MonoBehaviour
         gameIsPaused = false;
         pauseMenu.SetActive(false);
         loadManager.BackScene();
+    }
+
+    public void Failed()
+    {
+        isFailed = true;
+
+        resultScreen.SetActive(true);
+        scoreText.enabled = false;
+        mulText.enabled = false;
+        scoreManager.GetComponent<SongManager>().enabled = false;
+
+        okaysText.text = okayHits.ToString();
+        goodsText.text = goodHits.ToString();
+        perfectsText.text = perfectHits.ToString();
+        missesText.text = missedHits.ToString();
+
+        float totalHit = okayHits + goodHits + perfectHits;
+        float percentHit = (totalHit / totalNotes) * 100f;
+
+        percentHitText.text = percentHit.ToString("F1") + "%";
+        finalScoreText.text = currentScore.ToString();
     }
 }
